3c1bb5f6e63c6c75ea00b47119ee7b1021b22761
asm-saim <saim777m@gmail.com>
2026-09-25 20:44:12 +0600

    loading animation added

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex min-h-[60vh] items-center justify-center">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-slate-700 border-t-[#CCFF00]" />
+    </div>
+  );
+};
+
+export default Loading;
